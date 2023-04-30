8d1b15cbd70285695e7d365031d424c6e33a5b3a
rdias66 <dias.a@live.com>
2023-04-29 21:43:14 -0300

    first components added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Chat from '../../components/chat';
+import LoginForm from '../../components/login';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginForm />
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
